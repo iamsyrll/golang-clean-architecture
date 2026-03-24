1a7b8383693591e5b3f79bd09e065a19504c9aed
Syahrullah <syahrullah.mail@gmail.com>
2026-03-24 17:42:53 +0700

    chore(docs) add documentation

--- cmd/main.go
+++ cmd/main.go
@@ -9,10 +9,30 @@ import (
 	"golang-clean-arch/internal/usecase"
 	"golang-clean-arch/routes"
 
+	_ "golang-clean-arch/docs"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+//	@title			Swagger Example API
+//	@version		1.0
+//	@description	This is a sample API docs auto generate using swagger and gin framework.
+
+//	@contact.name	Clean Architecture API
+//	@contact.email	syahrullah.mail@gmail.com
+
+//	@host		localhost:8080
+//	@BasePath	/
+
+//	@securityDefinitions.apikey	ApiKeyAuth
+//	@in							header
+//	@name						Authorization
+//	@description				Description for what is this security definition being used
 
 func main() {
 	err := godotenv.Load()
@@ -38,6 +58,17 @@ func main() {
 		Router: r,
 		AuthUc: authUsecase,
 	})
+
+	r.GET("/docs", func(c *gin.Context) {
+		c.Redirect(302, "/docs/index.html")
+	})
+
+	authorized := r.Group("/docs", gin.BasicAuth(gin.Accounts{
+		"admin": "123456",
+	}))
+	{
+		authorized.GET("/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
 
 	log.Println("Server berjalan pada port :8080")
 	r.Run(":8080")
--- internal/delivery/http/handler/auth_handler.go
+++ internal/delivery/http/handler/auth_handler.go
@@ -13,6 +13,19 @@ type AuthHandler struct {
 	uc usecase.AuthUsecase
 }
 
+type AuthResponseLogin struct {
+	Message string `json:"message"`
+	Token   string `json:"token"`
+}
+
+type AuthResponseFailed struct {
+	Error string `json:"error"`
+}
+
+type RegisterResponse struct {
+	Message string `json:"message"`
+}
+
 func NewAuthHandler(r *gin.Engine, uc usecase.AuthUsecase) {
 	h := &AuthHandler{uc: uc}
 
@@ -20,6 +33,21 @@ func NewAuthHandler(r *gin.Engine, uc usecase.AuthUsecase) {
 	r.POST("/login", h.Login)
 }
 
+// Login godoc
+//
+//	@Summary		Login User
+//	@Description	Endpoint for user login
+//	@Tags			Auth
+//	@Accept			json
+//	@Produce		json
+//
+// @Param request body entity.LoginParams true "Login request"
+//
+//	@Success		200	{object}	AuthResponseLogin
+//	@Failure		400	{object}	AuthResponseFailed
+//	@Failure		404	{object}	AuthResponseFailed
+//	@Failure		500	{object}	AuthResponseFailed
+//	@Router			/login [post]
 func (h *AuthHandler) Login(c *gin.Context) {
 	var params entity.LoginParams
 
@@ -38,17 +66,36 @@ func (h *AuthHandler) Login(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "login success", "token": token})
 }
 
+// Register godoc
+//
+//	@Summary		Register User
+//	@Description	Endpoint for user register
+//	@Tags			Auth
+//	@Accept			json
+//
+// @Security ApiKeyAuth
+//
+// @Param request body entity.RegisterParams true "Register request"
+//
+//	@Produce		json
+//	@Success		200	{object}	RegisterResponse
+//	@Failure		400	{object}	AuthResponseFailed
+//	@Failure		404	{object}	AuthResponseFailed
+//	@Failure		500	{object}	AuthResponseFailed
+//	@Router			/register [post]
 func (h *AuthHandler) Register(c *gin.Context) {
 	var params entity.RegisterParams
 
 	err := c.ShouldBindBodyWithJSON(&params)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+		return
 	}
 
 	err = h.uc.Register(params.Username, params.Email, params.Password)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 
 	c.JSON(http.StatusCreated, gin.H{"message": "registered"})
